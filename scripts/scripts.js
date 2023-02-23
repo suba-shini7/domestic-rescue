9a36801c6ea15323ed58f0a9c62820f1efa4b2b3
Yasar <arafatharafath925@gmail.com>
2023-02-23 20:29:41 +0530

    My comit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,29 @@ function imgslide(){
     setTimeout("imgslide()",duration);
 }
 imgslide();
+
+
+
+var popimg=['./asset/popimg1.jfif' ,'./asset/popimg2.jpg' ,'./asset/popimg3.jpg','./asset/popimg4.jpg','./asset/popimg5.jpg'];
+var index=0;
+var duration=5000;
+
+function imgpop(){
+    document.getElementById("pop").src=popimg[index];
+    index++;
+
+    if(index==popimg.length){index=0}
+    setTimeout("imgpop()",duration);
+}
+imgpop();
+
+
+// Popup form function
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
