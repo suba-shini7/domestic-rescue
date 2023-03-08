a31008de8f7a639485e6617341c2d039aa48489b
Yasar <arafatharafath925@gmail.com>
2023-03-08 15:08:59 +0530

    Fix all issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,30 +26,7 @@ function imgpop(){
 }
 imgpop();
 
-
-// Popup form function
-
-// var modal = document.getElementById("mainpop");
-// var btn = document.getElementById("Savebtn");
-// var span = document.getElementsById("Close");
-// // When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   console.log(modal)
-//   modal.style.display = "block";
-//   console.log(modal)
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+//popup function 
 
 var audio = new Audio("./asset/btnSFX.wav");
 
@@ -65,3 +42,4 @@ function openPopup() {
     audio.play();
     
   }
+
